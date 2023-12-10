02cb3279bdd59dd5bbd7aaf876768ecf903fc81c
İsmail Gültekin <igultekin5@gmail.com>
2023-12-10 18:38:33 +0300

    theme configs and logout

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <span>Envanter Yönetim Sistemi</span>
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-end d-none d-sm-block">
+                    İsmail Gültekin - <script>document.write(new Date().getFullYear())</script>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
